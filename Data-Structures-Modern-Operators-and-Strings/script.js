589474a325d172cad41ebdaf699e84410cf88fdd
Can Arda <canarda.dev@gmail.com>
2023-09-20 23:20:41 +0200

    Add files via upload

--- Data-Structures-Modern-Operators-and-Strings/script.js
+++ Data-Structures-Modern-Operators-and-Strings/script.js
@@ -262,3 +262,34 @@ console.log(guests);
 // Nullish: null and undefined (NOT 0 or ''): 
 const guestCorrect = restaurant.numGuest ?? 10; 
 console.log(guestCorrect);
+
+const rest1 = { 
+  name: 'Capri',
+  numGuests: 20,
+}; 
+
+const rest2 = { 
+  name: 'La Piazza', 
+  owner: 'Giovanni Rossi',
+}; 
+
+// OR assigment operator 
+//rest1.numGuests = rest1.numGuests || 10; 
+//rest2.numGuests = rest2.numGuests || 10; 
+
+rest1.numGuests ||= 10;
+rest2.numGuests  ||= 10; 
+
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10; 
+rest2.numGuests ??= 10; 
+
+// AND assigment operator
+//rest1.owner = rest1.owner && '<ANONYMOUS>'; 
+//rest1.owner = rest1.owner && '<ANONYMOUS>'; 
+rest1.owner &&= '<ANONYMOUS>'; 
+rest2.owner &&= '<ANONYMOUS>'; 
+
+console.log(rest1);
+console.log(rest2);
